2cfee854c52358f87b5cf8f1a2f203cd2d00ab04
刘书鑫 <642545320@qq.com>
2021-09-14 16:20:26 +0800

    封装request请求

---
+++ anet/request.go
@@ -0,0 +1,15 @@
+package anet
+import "aurora/aiface"
+
+type Request struct {
+	conn aiface.IConnection //已经和客户端建立好的 链接
+	data []byte //客户端请求的数据
+}
+//获取请求连接信息
+func(r *Request) GetConnection() aiface.IConnection {
+	return r.conn
+}
+//获取请求消息的数据
+func(r *Request) GetData() []byte {
+	return r.data
+}
