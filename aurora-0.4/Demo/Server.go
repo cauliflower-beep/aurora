83d6557e8a0b1422c8addc322995ba73091e63db
lsx <642545320@qq.com>
2023-04-03 14:42:14 +0800

    集成全局配置

---
+++ aurora-0.4/Demo/Server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"aurora-v0.4/aiface"
+	"aurora-v0.4/anet"
+	"fmt"
+)
+
+func main() {
+	s := anet.NewServer() //创建一个server句柄
+
+	// 绑定一个路由处理业务
+	s.AddRouter(&PingRouter{})
+
+	s.Serve() //启动server
+}
+
+type PingRouter struct {
+	anet.BaseRouter //一定要先继承基础 BaseRouter
+}
+
+// PreHandle 前置业务
+func (pr *PingRouter) PreHandle(req aiface.IRequest) {
+	fmt.Println("Call Router PreHandle")
+	_, err := req.GetConnection().GetTCPConnection().Write([]byte("before ping ....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
+
+// Handle 主业务
+func (pr *PingRouter) Handle(req aiface.IRequest) {
+	fmt.Println("Call PingRouter Handle")
+	conn := req.GetConnection().GetTCPConnection()
+	data := req.GetData()
+	if _, err := conn.Write(data); err != nil {
+		fmt.Println("write back buf err|", err)
+	}
+}
+
+// PostHandle 后置业务
+func (pr *PingRouter) PostHandle(req aiface.IRequest) {
+	fmt.Println("Call Router PostHandle")
+	_, err := req.GetConnection().GetTCPConnection().Write([]byte("After ping .....\n"))
+	if err != nil {
+		fmt.Println("call back ping ping ping error")
+	}
+}
---
+++ aurora-0.4/aiface/iServer.go
@@ -0,0 +1,11 @@
+package aiface
+
+// IServer
+//  @Description: 定义服务接口
+type IServer interface {
+	Start() //启动服务器方法
+	Stop()  //停止服务器方法
+	Serve() //开启业务服务方法
+
+	AddRouter(router IRouter) //路由功能：给当前服务注册一个路由业务方法，供客户端连接处理使用
+}
